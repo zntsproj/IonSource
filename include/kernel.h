be75f44ed0a493d7873737a9f474f5629efdbda9
zntsproj <vseokaktusah7@gmail.com>
2025-01-18 08:20:16 +0000

    Fixing kernel.h

--- include/kernel.h
+++ include/kernel.h
@@ -1,3 +1,5 @@
+// kernel.h
+// Global fixes: on 2025-01-18, compile error (fix)
 #ifndef ION_KERNEL_H
 #define ION_KERNEL_H
 
@@ -13,29 +15,32 @@ void itoa(int value, char* str, int base);
 void printk(const char* format, ...) {
     va_list args;
     va_start(args, format);
-    
+
     const char* ptr = format;
-    
+
     while (*ptr != '\0') {
         if (*ptr == '%' && *(ptr + 1) == 's') {
             // Handle string format (%s)
             char* str = va_arg(args, char*);
-            vga_print_string(str, VGA_COLOR_WHITE);
+            printv(str);
             ptr += 2;
         } else if (*ptr == '%' && *(ptr + 1) == 'd') {
             // Handle integer format (%d)
             int num = va_arg(args, int);
             char buffer[20];
             itoa(num, buffer, 10);  // Convert number to string
-            vga_print_string(buffer, VGA_COLOR_WHITE);
+            printv(buffer);
             ptr += 2;
         } else {
             // Print a single character if not a format specifier
-            vga_print_char(*ptr, VGA_COLOR_WHITE);
+            volatile char *vga_buffer = (char *)VGA_TEXT_MODE_ADDR;
+            int pos = 0; // Replace this with logic to manage cursor position
+            vga_buffer[pos * 2] = *ptr;
+            vga_buffer[pos * 2 + 1] = 0x07;
             ptr++;
         }
     }
-    
+
     va_end(args);
 }
 
@@ -43,22 +48,22 @@ void printk(const char* format, ...) {
 void itoa(int value, char* str, int base) {
     int i = 0;
     int sign = value;
-    
+
     if (sign < 0) {
         value = -value;
     }
-    
+
     // Convert integer to string
     do {
         str[i++] = value % base + '0';
     } while ((value /= base) > 0);
-    
+
     if (sign < 0) {
         str[i++] = '-';
     }
-    
+
     str[i] = '\0';
-    
+
     // Reverse the string
     int start = 0;
     int end = i - 1;
